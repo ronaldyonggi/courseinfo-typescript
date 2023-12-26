7cc9a41599a2b11f7c659467d06a3f2f0205eae3
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-26 15:26:20 +0800

    9.14 created Total component

---
+++ src/components/Total.tsx
@@ -0,0 +1,13 @@
+interface TotalProps {
+    total: number
+}
+
+const Total = (props: TotalProps): JSX.Element => {
+    return (
+        <p>
+            Number of exercises: {props.total}
+        </p>
+    )
+}
+
+export default Total
