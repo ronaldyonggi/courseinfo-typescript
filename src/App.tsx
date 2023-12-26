3e2555ad2716c4c3d3b1f43cd14ac7225ab3b653
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-26 15:26:39 +0800

    9.14 parts broken into separate components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Total from "./components/Total";
+
 const App = () => {
   const courseName = "Half Stack Application Development";
   const courseParts = [
@@ -19,19 +23,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>
-        Number of exercises {totalExercises}
-      </p>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts}/>
+      <Total total={totalExercises} />
     </div>
   )
 }
