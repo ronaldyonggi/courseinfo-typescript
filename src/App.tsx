56f4a2707337f0381d86b08b168682a1b33e29c5
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-27 13:00:27 +0800

    import CoursePart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Total from "./components/Total";
+import { CoursePart } from "./types";
 
 const App = () => {
   const courseName = "Half Stack Application Development";
