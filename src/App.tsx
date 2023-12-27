e4e56c64b13ebdb8b0a2e428763cfedd55bb6366
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-27 13:47:08 +0800

    9.15 add a "special" course part

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,13 @@ const App = () => {
     description: "a hard part",
     kind: "basic",
     },
+    {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    kind: "special"
+    }
   ];
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
